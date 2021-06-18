59bacdf21ad8c095d8004825233b9f44391415cf
Gatlee Kaw <gatlee.kaw@gmail.com>
2021-06-18 20:38:24 +1000

    Remove emacs
    Because we manage it with home-manager

--- configuration.nix
+++ configuration.nix
@@ -82,7 +82,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     firefox
-    emacs
     zsh
     xcape
     discord
