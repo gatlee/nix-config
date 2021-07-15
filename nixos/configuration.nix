1c770365fbba67d7944705072bc0ae47d6536b8c
Gatlee Kaw <gatlee.kaw@gmail.com>
2021-07-15 23:45:43 +1000

    Remove zsh from configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,7 +85,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     firefox
-    zsh
     xcape
     discord
     droidcam
